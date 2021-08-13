78f1abc82fac413296dac3aab72876169f70abeb
Igor Andrade <55281934+igorandrade12@users.noreply.github.com>
2021-08-13 09:26:00 -0300

    Pasta Cards Diversos Adicionada

---
+++ Cards Diversos/gulp.js
@@ -0,0 +1,27 @@
+vargulp=require(‘gulp’);
+
+varsass=require(‘gulp-sass’);
+
+gulp.task(‘sass’,function(){
+
+gulp.src(‘./scss’)
+
+.pipe(sass())
+
+.pipe(gulp.dest(function(f){
+
+return (f.base+”\CSSFile”);
+
+}))
+
+});
+
+ 
+
+gulp.task(‘default’, [‘sass’],function(){
+
+gulp.watch(‘./PATH/*scss’, [‘sass’]);
+
+})
+
+Note
